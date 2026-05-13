67358832eafad020bbb2c72190a4914182bd261c
qamarelayadi1-cmd <qamarelayadi1@gmail.com>
2026-05-13 16:31:12 +0100

    Ajout d'une interaction au clic

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log("JavaScript fonctionne !");
+const btnMessage = document.getElementById("btnMessage");
+const message = document.getElementById("message");
+
+btnMessage.addEventListener("click", function () {
+  message.textContent = "Bravo, tu as cliqué sur le bouton !";
+});
